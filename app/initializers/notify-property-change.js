15459fa988680064099fec07322bf9da57951a1d
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-08-24 01:26:28 +0500

    export initializer and service for app

---
+++ app/initializers/notify-property-change.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-data-stop-infinite-retry-for-failed-relationship-fetches/initializers/notify-property-change';
---
+++ app/services/cache-response.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-data-stop-infinite-retry-for-failed-relationship-fetches/services/cache-response';
