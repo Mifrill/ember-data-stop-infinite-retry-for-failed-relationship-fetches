395fdbd60ff1ff646a6b338ad44b2a066531f73b
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-08-25 02:45:17 +0500

    reproduce bug for following upgrades (3.7 - 3.11)

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,70 +1,60 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-
 module.exports = function() {
-  return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary'),
-  ]).then((urls) => {
-    return {
-      scenarios: [
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[0]
-            }
-          }
-        },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1]
-            }
-          }
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2]
-            }
-          }
-        },
-        {
-          name: 'ember-default',
-          npm: {
-            devDependencies: {}
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-3.7',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.7.0',
+            'ember-data': '~3.7.0',
           }
         }
-      ]
-    };
-  });
+      },
+      {
+        name: 'ember-lts-3.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.8.0',
+            'ember-data': '~3.8.0',
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.9',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.9.0',
+            'ember-data': '~3.9.0',
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.10',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.10.0',
+            'ember-data': '~3.10.0',
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.11',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.11.0',
+            'ember-data': '~3.11.0',
+          }
+        }
+      },
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {}
+        }
+      }
+    ]
+  };
 };
