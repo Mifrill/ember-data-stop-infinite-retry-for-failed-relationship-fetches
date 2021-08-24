5f89983715aa1ede6895254949632ed97f411180
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2021-08-24 14:41:30 +0500

    rewrite mirage endpoints to reproduce bug

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,6 +1,8 @@
+import Response from 'ember-cli-mirage/response';
+
 export default function() {
   this.get('/users');
-  this.get('/users/:id');
-  this.get('/notes');
-  this.get('/notes/:id');
+  this.get('/notes/:id', function() {
+    return new Response(404);
+  });
 }
